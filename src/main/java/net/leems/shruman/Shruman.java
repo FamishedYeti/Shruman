d0f418a5acbee9b0f4da160c59858403190012b0
BuildTools <unconfigured@null.spigotmc.org>
2022-07-19 23:03:57 -0400

    added \\test

--- src/main/java/net/leems/shruman/Shruman.java
+++ src/main/java/net/leems/shruman/Shruman.java
@@ -4,6 +4,8 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+\\ test
+
 public class Shruman implements ModInitializer {
 	public static final String MOD_ID = "shruman";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
